d03137c1ccee534d8c2c0eb2b4f824361be1f66e
amgst <amgst99@gmail.com>
2025-09-25 16:50:41 +0500

    sd

--- server/routes.ts
+++ server/routes.ts
@@ -1,6 +1,6 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
-import { storage } from "./storage";
+import { storage } from "../api/lib/storage";
 import { insertSectionSchema, insertInstallationSchema } from "@shared/schema";
 import { z } from "zod";
 
